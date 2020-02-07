a8c5d834d1e4ca8cfa9bcded6b16847006b8a3e0
AbstractFear <lugia20102010@gmail.com>
2020-02-07 09:33:12 +0300

    some cnahges in roles

--- contracts/Users.sol
+++ contracts/Users.sol
@@ -1,19 +1,20 @@
 pragma experimental ABIEncoderV2;
 
-import ".././node_modules/openzeppelin/contracts/access/roles/MainAdminRole.sol";
-import ".././node_modules/openzeppelin/contracts/access/roles/AdminRole.sol";
-import ".././node_modules/openzeppelin/contracts/access/roles/MailWorkerRole.sol";
-import ".././node_modules/openzeppelin/contracts/ownership/Ownable.sol";
+import "../node_modules/@openzeppelin/contracts/access/roles/MainAdminRole.sol";
+import "../node_modules/@openzeppelin/contracts/access/roles/AdminRole.sol";
+import "../node_modules/@openzeppelin/contracts/access/roles/MailWorkerRole.sol";
+import "../node_modules/@openzeppelin/contracts/ownership/Ownable.sol";
 // import "./MailService.sol";
 
 /**
     @dev Создание контракта UsersContract. Контракт UsersContract хранит
     структуру пользователей, которые регистрируются в блокчейн-системе.
     Структура содержит в себе личные данные о пользователе, домашний адрес
-    и роль, котораябыла ему выдана. По умолчанию равна "Пользователь".
+    и роль, которая была ему выдана. По умолчанию равна "Пользователь".
     Является константой.
  */
 contract UsersContract is MainAdminRole, AdminRole, MailWorkerRole, Ownable {
+    using Roles for Roles.Role;
 
     struct usersInfo{
         address account;
@@ -22,67 +23,60 @@ contract UsersContract is MainAdminRole, AdminRole, MailWorkerRole, Ownable {
         string secondName;
         string patronym;
         string homeAddr;
-        string role;
     }
 
     string constant roleOf = 'Пользователь';
     /**
-        @dev Маппинг балансов. Хранит в себе данные о денежных стредствах контрактах.
+     * @dev Маппинг балансов. Хранит в себе данные о денежных стредствах контрактах.
      */
     mapping(address => uint) internal balancesAll;
     /**
-        @dev Маппинг идентификатора отделения. Хранит в себе номер
-        отделения, в котором работает сотрудник.
+     * @dev Маппинг идентификатора отделения. Хранит в себе номер
+     * отделения, в котором работает сотрудник.
      */
     mapping(address => string) public numberOfMailWorker;
     /**
-        @dev Маппинг с пользователями. Ключевое значение uint является индексом пользователя
+     * @dev Маппинг с пользователями. Ключевое значение uint является индексом пользователя
      */
     mapping(uint => usersInfo) public users;
     /**
-        @dev Маппинг пользователей для выборки пользователя по адресу кошелька
+     * @dev Маппинг пользователей для выборки пользователя по адресу кошелька
      */
     mapping(address => usersInfo) public usersByAddress;
     /**
-        @dev Маппинг пользователей для выборки пользователя по роли, назначенной ему
+     * @dev Маппинг пользователей для выборки пользователя по роли, назначенной ему
      */
     mapping(string => usersInfo) public usersByRole;
-    /** 
-        @dev Количество пользователей в структуре
-    */
+    /**
+     * @dev Количество пользователей в структуре
+     */
     uint public usersCount;
 
     /**
-        @dev Ивент о регистрации нового пользователя. 
+     * @dev Ивент о регистрации нового пользователя.
      */
     event NewUser(address account, string name, string secondName, string patronym, string homeAddr, string role);
-
     /**
-        @dev Конструктор. При вызове метода deploy он заносит в базу данных главного админа
-        со значениями, указанными ниже. 
+     * @dev Конструктор. При вызове метода deploy он заносит в базу данных главного админа
+     * со значениями, указанными ниже.
      */
     constructor () public {
         address _owner = msg.sender;
-        string memory _role = "Главный администратор";
         usersCount++;
-        usersInfo(_owner, sha256(abi.encode('admin')), 'admin', 'admin', 'admin', 'Санкт-Петербург, ул. Мира, д. 6', _role);
-        users[usersCount] = usersInfo(_owner, '', 'admin', 'admin', 'admin', 'Санкт-Петербург, ул. Мира, д. 6', _role);
-        usersByAddress[_owner] = usersInfo(_owner, '', 'admin', 'admin', 'admin', 'Санкт-Петербург, ул. Мира, д. 6', _role);
-        usersByRole[_role] = usersInfo(_owner, '', 'admin', 'admin', 'admin', 'Санкт-Петербург, ул. Мира, д. 6', _role);
+        usersInfo(_owner, sha256(abi.encode('admin')), 'admin', 'admin', 'admin', 'Санкт-Петербург, ул. Мира, д. 6');
+        users[usersCount] = usersInfo(_owner, '', 'admin', 'admin', 'admin', 'Санкт-Петербург, ул. Мира, д. 6');
+        usersByAddress[_owner] = usersInfo(_owner, '', 'admin', 'admin', 'admin', 'Санкт-Петербург, ул. Мира, д. 6');
     }
-
     /**
-        @dev Предварительная функция регистрации аккаунта. При регистрации нового кошелька пользователь заносится в структуру, чтобы 
-        потом взаимодействовать с сайтом. Также присваивает зарегистрированным пользователям роль 
-        "Пользователь"
-        @return bool 
-    */
-    function _register(address _account, string memory _password, string memory _name, string memory _secondName, string memory _patronym, string memory _homeAddr, string memory _role) private returns (bool success){
+     *   @dev Предварительная функция регистрации аккаунта. При регистрации нового кошелька пользователь заносится в структуру, чтобы
+     *   потом взаимодействовать с сайтом. Также присваивает зарегистрированным пользователям роль "Пользователь"
+     *   @return bool
+     */
+    function _register(address _account, string memory _password, string memory _name, string memory _secondName, string memory _patronym, string memory _homeAddr) private returns (bool success){
         usersCount++;
         balancesAll[_account] = _account.balance;
-        users[usersCount] = usersInfo(_account, sha256(abi.encodePacked(_password)), _name, _secondName, _patronym, _homeAddr, _role);
-        usersByAddress[_account] = usersInfo(_account, sha256(abi.encodePacked(_password)), _name, _secondName, _patronym, _homeAddr, _role);
-        usersByRole[_role] = usersInfo(_account, sha256(abi.encodePacked(_password)), _name, _secondName, _patronym, _homeAddr, _role);
+        users[usersCount] = usersInfo(_account, sha256(abi.encodePacked(_password)), _name, _secondName, _patronym, _homeAddr);
+        usersByAddress[_account] = usersInfo(_account, sha256(abi.encodePacked(_password)), _name, _secondName, _patronym, _homeAddr);
         emit NewUser(_account, _name, _secondName, _patronym, _homeAddr, roleOf);
         return true;
     }
@@ -92,7 +86,10 @@ contract UsersContract is MainAdminRole, AdminRole, MailWorkerRole, Ownable {
         @return bool
      */
 
-    function registration(address _account, string memory _password, string memory name, string memory secondName, string memory patronym, string memory _homeAddr) public returns (bool success){
+    function registration(address _account, string memory _password, string memory name, string memory secondName, string memory patronym, string memory _homeAddr)
+        public
+        returns(bool success)
+    {
         usersInfo[] memory usersInf = new usersInfo[](usersCount);
         address account;
         for(uint i = 0; i < usersCount; i++){
@@ -106,8 +103,8 @@ contract UsersContract is MainAdminRole, AdminRole, MailWorkerRole, Ownable {
     }
 
     /**
-        @dev Функция валидации аккаунта. Для авторизации зарегистрированного пользователя. 
-        @return bool
+     * @dev Функция валидации аккаунта. Для авторизации зарегистрированного пользователя.
+     * @return bool
      */
 
     function validation(address _account, string memory _password) public view returns (bool success) {
